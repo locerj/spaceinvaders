e95cdd211abd5823f685d71d8a3cd3d72a5653cc
Marcelo Souza <locerj32@gmail.com>
2024-11-12 18:14:44 -0300

    adicionado a lógia dos invaders

---
+++ src/classes/Grid.js
@@ -0,0 +1,78 @@
+import Invader from "./Invader.js";
+
+class Grid {
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.direction = "right";
+        this.moveDown = false;
+        
+        this.invadersVelocity = 1;
+        this.invaders = this.init();
+    }
+
+    init() {
+        const array = [];
+        for (let row = 0; row < this.rows; row += 1) {
+            for (let col = 0; col < this.cols; col += 1) {
+                const invader = new Invader(
+                    {
+                        x: col * 60 + 20,
+                        y: row * 47 + 20,
+                    },
+                    this.invadersVelocity
+                );
+                array.push(invader);
+            }
+        }
+        return array;
+    }
+
+    draw(ctx) {
+        this.invaders.forEach(invader => invader.draw(ctx));
+    }
+
+    update(){
+        if(this.reachedRightBoundary()){
+            this.direction = "left";
+            this.moveDown = true;
+        } else if(this.reachedLeftBoundary()) {
+            this.direction = "right";
+            this.moveDown = true;
+        }
+
+        this.invaders.forEach((invader) => {
+
+            if(this.moveDown) {
+                invader.moveDown();
+                invader.incrementVelocity(0.1);
+                this.invadersVelocity = invader.velocity;
+            }
+
+            if(this.direction === "right") invader.moveRight();
+            if(this.direction === "left") invader.moveLeft();
+        });
+        this.moveDown = false;
+    }
+   
+    reachedRightBoundary(){
+        return this.invaders.some(
+            (invader) => invader.position.x + invader.width >= innerWidth
+        );
+    }
+
+    reachedLeftBoundary(){
+        return this.invaders.some(
+            (invader) => invader.position.x <= 0
+        );
+    }
+
+    getRandomInvader() {
+        const index = Math.floor(Math.random() * this.invaders.length);
+        return this.invaders[index];
+    }
+
+
+}
+
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Grid from "./classes/Grid.js";
+import Invader from "./classes/Invader.js";
 import Player from "./classes/Player.js";
 import Projectile from "./classes/Projectile.js";
 
@@ -10,54 +12,77 @@ canvas.height = innerHeight;
 ctx.imageSmoothingEnabled = false;
 
 const player = new Player(canvas.width, canvas.height);
+const grid = new Grid(3, 6);
+
 const playerProjectiles = [];
+const invaderProjectiles = [];
+
 
 
 const keys = {
     left: false,
     right: false,
     shoot: {
-        pressed:false,
-        released:true,
+        pressed: false,
+        released: true,
     },
 };
 
-const drawProjectiles = ( )=> {
-    playerProjectiles.forEach((projectile) => {
+const drawProjectiles = () => {
+    const projectiles = [...playerProjectiles, ...invaderProjectiles];
+    projectiles.forEach((projectile) => {
         projectile.draw(ctx);
         projectile.update();
     })
 }
 
-const clearProjectiles = ()=> {
-    playerProjectiles.forEach((projectile, index) =>{
-        if(projectile.position.y <= 0){
+const clearProjectiles = () => {
+    playerProjectiles.forEach((projectile, index) => {
+        if (projectile.position.y <= 0) {
             playerProjectiles.splice(index, 1);
         }
     });
 }
 
+const checkShootInvaders = () => {
+    grid.invaders.forEach((invader, invaderIndex) => {
+        playerProjectiles.some((projectile, projectileIndex) => {
+            if (invader.hit(projectile)) {
+                grid.invaders.splice(invaderIndex, 1);
+                playerProjectiles.splice(projectileIndex, 1);
+            }
+        });
+    });
+}
+
 const gameLoop = () => {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    console.log(playerProjectiles)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
     drawProjectiles();
     clearProjectiles();
+
+    checkShootInvaders();
+
+    grid.draw(ctx);
+    grid.update();
 
     ctx.save();
 
     ctx.translate(player.position.x + player.width / 2, player.position.y + player.height / 2);
 
-    if(keys.shoot.pressed && keys.shoot.released){
+    if (keys.shoot.pressed && keys.shoot.released) {
         player.shoot(playerProjectiles);
         keys.shoot.released = false;
     }
 
-    if(keys.left && player.position.x >= 0){
+    if (keys.left && player.position.x >= 0) {
         player.moveLeft();
         ctx.rotate(-0.15);
     }
 
-    if(keys.right && player.position.x <= canvas.width - player.width){
+    if (keys.right && player.position.x <= canvas.width - player.width) {
         player.moveRight();
         ctx.rotate(0.15);
     }
@@ -72,21 +97,29 @@ const gameLoop = () => {
 
 addEventListener("keydown", (event) => {
     const key = event.key.toLowerCase();
-    if(key === "a") keys.left = true;
-    if(key === "d") keys.right = true;
-    if(key == "enter") keys.shoot.pressed = true;
-    
+    if (key === "a") keys.left = true;
+    if (key === "d") keys.right = true;
+    if (key == "enter") keys.shoot.pressed = true;
+
 });
 
 addEventListener("keyup", (event) => {
     const key = event.key.toLowerCase();
 
-    if(key === "a") keys.left = false;
-    if(key === "d") keys.right = false;
-    if(key === "enter") {
+    if (key === "a") keys.left = false;
+    if (key === "d") keys.right = false;
+    if (key === "enter") {
         keys.shoot.pressed = false;
         keys.shoot.released = true;
-    } 
+    }
 });
+
+// setInterval(() => {
+//     const invader = grid.getRancomInvader();
+
+//     if (invader){
+//         invader.shoot(invaderProjectiles);
+//     }
+// }, 1000)
 
 gameLoop();
